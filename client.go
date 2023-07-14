39db28732c30da2d28c7dff83f1220dc21bfdcd3
Alina <138588984+timothysaputra@users.noreply.github.com>
2023-07-14 17:02:04 +0300

    Create client.go

---
+++ client.go
@@ -0,0 +1,4 @@
+// Client is an interface for the web3 RPC API.
+type Client interface {
+	// GetBalance returns the balance for an address at the given block number (nil for latest).
+	GetBalance(ctx context.Context, address string, blockNumber *big.Int) (*big.Int, error)
