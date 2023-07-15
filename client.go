6e97c54b23721e620fa2a6925c2b059b7b18c4cc
Alina <138588984+timothysaputra@users.noreply.github.com>
2023-07-15 15:02:06 +0300

    Update client.go

--- client.go
+++ client.go
@@ -2,3 +2,5 @@
 type Client interface {
 	// GetBalance returns the balance for an address at the given block number (nil for latest).
 	GetBalance(ctx context.Context, address string, blockNumber *big.Int) (*big.Int, error)
+// GetCode returns the code for an address at the given block number (nil for latest).
+	GetCode(ctx context.Context, address string, blockNumber *big.Int) ([]byte, error)
