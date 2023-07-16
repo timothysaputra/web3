dd6d859f31093533af7765ec2fb16a61ae7ed91f
Alina <138588984+timothysaputra@users.noreply.github.com>
2023-07-16 13:54:24 +0300

    Update client.go

--- client.go
+++ client.go
@@ -4,3 +4,5 @@ type Client interface {
 	GetBalance(ctx context.Context, address string, blockNumber *big.Int) (*big.Int, error)
 // GetCode returns the code for an address at the given block number (nil for latest).
 	GetCode(ctx context.Context, address string, blockNumber *big.Int) ([]byte, error)
+GetBlockByNumber(ctx context.Context, number *big.Int, includeTxs bool) (*Block, error)
+	// GetBlockByHash returns block details for the given hash, optionally include full transaction details.
