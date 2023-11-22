820dea956c1cdd3671055d1dd95461767fa71231
Alina <138588984+timothysaputra@users.noreply.github.com>
2023-11-22 12:25:26 +0200

    Update client.go

--- client.go
+++ client.go
@@ -58,3 +58,8 @@ var result hexutil.Bytes
 	err := c.r.CallContext(ctx, &result, "eth_call", toCallArg(msg), "latest")
 if err != nil {
 		return nil, err
+}
+	return NewClient(r), nil
+}
+
+// NewClient returns a new client backed by an existing rpc.Client.
