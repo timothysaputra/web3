2913b4d34b8a5e59730ada0bd78c4dad01e54225
Alina <138588984+timothysaputra@users.noreply.github.com>
2023-11-30 12:59:08 +0200

    Update client.go

--- client.go
+++ client.go
@@ -77,3 +77,5 @@ c.r.Close()
 }
 
 func (c *client) Call(ctx context.Context, msg CallMsg) ([]byte, error) {
+var result hexutil.Bytes
+	err := c.r.CallContext(ctx, &result, "eth_call", toCallArg(msg), "latest")
